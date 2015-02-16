6c1ebc08d406497b729c623efbc7a0ac464122a9
TarrellRod <tarrellrodrigues@gmail.com>
2015-02-15 18:59:30 -0700

    file struct
    better file structure added router

---
+++ js/AfricaApp.js
@@ -0,0 +1,9 @@
+var app = angular.module("AfricaApp", ['ngRoute']).config(function($routeProvider) {
+        $routeProvider.when('/flights', {
+            templateUrl: 'views/flights.html',
+            controller:'Controller'
+        }).when('/bus', {
+            templateUrl: 'views/bus.html',
+            controller: 'Controller'
+        });
+    });
